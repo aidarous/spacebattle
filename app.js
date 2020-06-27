07a147b6911adce0eb3f5daefd050a5dcabea3e6
Student_MA102 <aidarous12@gmail.com>
2020-06-27 12:19:41 -0400

    USS Schwarzeneggar and Alien ship stats are set up

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
-prompt()
+/* Pseudo Code
+* Create alien ship and set properties
+* - hull is between 3 and 6 
+* - use math.random() to define accuracy between .6 and .8
+* - let firepower = 5 
+* Create USS Schwarzenegger object and set properties
+* - hull = 20
+* - firepower = 5
+* - accuracy = random number
+* make function for attack and retreat
+* get the ships to battle each other
+*/
 
-alert()
+/* USS Schwarzenegger and its stats */
+let ussSchwarzenegger = { 
+    hull: 20,
+    firepower: 5,
+    accuracy: .7,
+    fire: function () {
+       let damageDealt = Math.floor(Math.random()*3)
+       console.log(`You dealt ${damageDealt} damage`)
+    }
+}
+
+console.log(ussSchwarzenegger)
+// ussSchwarzenegger.fire()
+/* alien ship class*/
+// since there will be 6 alien ships a class is used to initialize
+class AlienShip {
+   constructor(){
+       this.hull = Math.floor(Math.random()*4)+3;
+       this.firepower = Math.floor(Math.random()*3)+2;
+       this.accuracy = Math.floor(Math.random()*3)+6/10;
+   }
+}
+
+const alienOne = new AlienShip()
+console.log(alienOne)
+
+let decision =() => {
+    let decide = prompt("Press 1 to Attack or 2 to Retreat")
+    if (decide = 1){
+    alert("You chose attack!")
+    }
+    else if (decide = 2){
+        alert("You chose retreat")
+    }
+
+}
+
+//decision()
