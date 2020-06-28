c931e413bdd6c01edeb2e35131a13d97530ce14e
Student_MA102 <aidarous12@gmail.com>
2020-06-28 14:39:03 -0400

    Uss Ship and Alien Ship can now attack one another

--- app.js
+++ app.js
@@ -22,30 +22,59 @@ let ussSchwarzenegger = {
     }
 }
 
-console.log(ussSchwarzenegger)
-// ussSchwarzenegger.fire()
+
 /* alien ship class*/
 // since there will be 6 alien ships a class is used to initialize
 class AlienShip {
    constructor(){
-       this.hull = Math.floor(Math.random()*4)+3;
-       this.firepower = Math.floor(Math.random()*3)+2;
-       this.accuracy = Math.floor(Math.random()*3)+6/10;
+       this.hull = Math.floor(Math.random()*4)+3; // randomizes hull to be between 3 and 6
+       this.firepower = Math.floor(Math.random()*3)+2; // randomizes firepower between 2 and 4
+       this.accuracy = Math.floor(Math.random()*3)+6/10; // randomizes accuracy betweem .6 and .8
+   }
+   alienFire() {
+       let alienDamageDealt = this.firepower
+       return alienDamageDealt; 
    }
 }
 
+
 const alienOne = new AlienShip()
-console.log(alienOne)
+// console.log(alienOne) troubleshooting to make sure object was created
+let remainingHull = alienOne["hull"] // created a variable for alien hull to use freely in code
+let ussHull = ussSchwarzenegger["hull"] // created to use the USS hull conveniently
+//Pseudo code
+// - begin the fight with the decision to fight or retreat
+// - create a loop where user chooses to fight or retreat
+//      - attacking continues the loop and the user and alien each attack one another
+//      - retreating exits the loop and the game ends
 
-let decision =() => {
-    let decide = prompt("Press 1 to Attack or 2 to Retreat")
-    if (decide = 1){
-    alert("You chose attack!")
-    }
-    else if (decide = 2){
-        alert("You chose retreat")
-    }
+//alert("Welcome to Spacebattle! Are you ready to save the universe?")
 
+let decide = prompt("Press 1 to start game or 2 to quit")
+
+        if (decide == 1){
+            while(remainingHull > 0){
+                let battleDecision = prompt("What will you do? Press 1 to attack or 2 to run away")
+                if(battleDecision == 1 && remainingHull >= 0){
+
+                    ussSchwarzenegger.fire()
+                    alienOne.alienFire()
+                    remainingHull -= ussSchwarzenegger["firepower"];
+                    ussHull -= alienOne["firepower"]
+                    console.log(`You chose attack! alien ship has ${remainingHull} health and you have ${ussHull}`)
+                } else if(battleDecision == 2){
+                    alert("You ran away from battle")
+                    break
+                } else{
+                    alert("You have defeated Alien ship 1")
+                }
+            }
+
+        }
+        else if (decide == 2){
+        alert("You have quit the game")
+        
+        }
+if (remainingHull <= 0){
+    alert("YOU WIN!")
 }
-
-//decision()
