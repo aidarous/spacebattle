ffbc68b66fa17bda7aa824c52d3c2af38d2bfb26
Student_MA102 <aidarous12@gmail.com>
2020-06-29 01:01:03 -0400

    Turn based fighting now functional

--- app.js
+++ app.js
@@ -17,64 +17,92 @@ let ussSchwarzenegger = {
     firepower: 5,
     accuracy: .7,
     fire: function () {
-       let damageDealt = Math.floor(Math.random()*3)
-       console.log(`You dealt ${damageDealt} damage`)
+       let minDamageDealt = (Math.floor(3) * Math.random());
+       let maxDamageDealt = (Math.floor(9) * Math.random());
+       let damageDealt = Math.random() * (maxDamageDealt - minDamageDealt) +minDamageDealt;
+       damageDealt = Math.ceil(damageDealt);
+       console.log(`You dealt ${damageDealt} damage`);
     }
 }
+
 
 
 /* alien ship class*/
 // since there will be 6 alien ships a class is used to initialize
 class AlienShip {
    constructor(){
-       this.hull = Math.floor(Math.random()*4)+3; // randomizes hull to be between 3 and 6
-       this.firepower = Math.floor(Math.random()*3)+2; // randomizes firepower between 2 and 4
-       this.accuracy = Math.floor(Math.random()*3)+6/10; // randomizes accuracy betweem .6 and .8
-   }
-   alienFire() {
-       let alienDamageDealt = this.firepower
-       return alienDamageDealt; 
+       this.hull = (Math.floor(Math.random()*4)+3); // randomizes hull to be between 3 and 6
+       this.firepower = (Math.floor(Math.random()*3)+2); // randomizes firepower between 2 and 4
+       this.accuracy = (Math.floor(Math.random()*3)+6/10); // randomizes accuracy betweem .6 and .8
    }
 }
 
 
-const alienOne = new AlienShip()
-// console.log(alienOne) troubleshooting to make sure object was created
-let remainingHull = alienOne["hull"] // created a variable for alien hull to use freely in code
+const alienShip = new AlienShip()
+let chanceOfHitting;
+let remainingHull = alienShip["hull"] // created a variable for alien hull to use freely in code
 let ussHull = ussSchwarzenegger["hull"] // created to use the USS hull conveniently
+let aliensToGo = 6
 //Pseudo code
 // - begin the fight with the decision to fight or retreat
 // - create a loop where user chooses to fight or retreat
 //      - attacking continues the loop and the user and alien each attack one another
 //      - retreating exits the loop and the game ends
+// - create conditions for when USS attack hits and alien hits
+// - create conditions for when USS attack hits and alien misses
+// - create conditions for when USS attack misses and alien hits
+// - create conditions for when USS attack misses and alien misses
 
 //alert("Welcome to Spacebattle! Are you ready to save the universe?")
 
-let decide = prompt("Press 1 to start game or 2 to quit")
 
-        if (decide == 1){
-            while(remainingHull > 0){
-                let battleDecision = prompt("What will you do? Press 1 to attack or 2 to run away")
-                if(battleDecision == 1 && remainingHull >= 0){
+let decide = prompt("Press 1 to start game or 2 to quit");
 
-                    ussSchwarzenegger.fire()
-                    alienOne.alienFire()
-                    remainingHull -= ussSchwarzenegger["firepower"];
-                    ussHull -= alienOne["firepower"]
-                    console.log(`You chose attack! alien ship has ${remainingHull} health and you have ${ussHull}`)
-                } else if(battleDecision == 2){
+        if (decide == "1"){
+        while(aliensToGo > 0){
+        console.log(aliensToGo)
+            
+                chanceOfHitting = Math.random()
+                console.log(chanceOfHitting)
+                let battleDecision = prompt("What will you do? Press 1 to attack or 2 to run away");
+                if(battleDecision == "1" && remainingHull >= 0){
+                    // when USS attack hits and alien hits
+                    if( ussSchwarzenegger["accuracy"] <= chanceOfHitting) {
+                        
+                        remainingHull -= ussSchwarzenegger["firepower"];
+                        alert(`Your attack was successful! alien ship has ${remainingHull} health`);
+                        if(remainingHull <= 0){
+                            alert("Congratulations! You destroyed the alien ship")
+                            alienShip = new AlienShip();
+                            alert("You have defeated Alien ship ")
+                            aliensToGo --;
+                            console.log(aliensToGo)
+                        }
+                        // when USS attack hits and alien misses
+                    } else{
+                        alert("You missed!")
+                    }
+                    
+                    if (alienShip["accuracy"] >= chanceOfHitting){
+
+                        alert("alien is going to attack")
+                        ussHull -= alienShip["firepower"];
+                        alert(`Nice work! Your attack was successful while the alien ship missed! alien ship has ${remainingHull} health`)
+                    }
+                    
+                } else if(battleDecision == "2"){
                     alert("You ran away from battle")
                     break
-                } else{
-                    alert("You have defeated Alien ship 1")
-                }
-            }
+                } 
 
+            
+            
         }
+    }
         else if (decide == 2){
         alert("You have quit the game")
         
         }
 if (remainingHull <= 0){
-    alert("YOU WIN!")
+    alert("YOU WIN!") 
 }
